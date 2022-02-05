129a3044f1e193caabaf300aaad5e7164189182f
estebanhirzfeld <estebanhirzfeld@gmail.com>
2022-02-04 21:46:49 -0300

    Casi terminado

--- Js/main.js
+++ Js/main.js
@@ -1,1 +1,231 @@
-$('.carousel').bcSwipe({ threshold: 50 });
+// $('.carousel').bcSwipe({ threshold: 50 });
+
+let isEducation = true;
+
+Education();
+
+function Education() {
+    $('.qualification__content').empty();
+    $('.qualification__content').append(
+        `
+        <div class="col-4 col-md-4 col-lg-4 ">
+                                <div class="leftColumn cardBackground alignCenter">
+                                    <p>Técnico Electrónico<br>
+                                        Instituto San Bonifacio</p>
+                                </div>
+                            </div>
+                            <div class="col-4 col-md-4 col-lg-4 ">
+                                <div class="middleColumn">
+                                    <div class="rule"></div>
+                                    <div class="ruleDot"></div>
+                                    <div class="rule"></div>
+                                </div>
+                            </div>
+                            <div class="col-4 col-md-4 col-lg-4 ">
+                                <div class="RightColumn">
+                                </div>
+                            </div>
+                            <div class="col-4 col-md-4 col-lg-4 ">
+                                <div class="leftColumn">
+                                </div>
+                            </div>
+                            <div class="col-4 col-md-4 col-lg-4 ">
+                                <div class="middleColumn">
+                                    <div class="rule"></div>
+                                    <div class="ruleDot"></div>
+                                    <div class="rule"></div>
+                                </div>
+                            </div>
+                            <div class="col-4 col-md-4 col-lg-4 ">
+                                <div class="RightColumn cardBackground alignCenter">
+                                    <p>Ingeniería En Sistemas<br>
+                                        UTN - University </p>
+                                </div>
+                            </div>
+                            <div class="col-4 col-md-4 col-lg-4 ">
+                                <div class="leftColumn cardBackground alignCenter">
+                                    <p>Front-End Developer<br>
+                                        Coder House</p>
+                                </div>
+                            </div>
+                            <div class="col-4 col-md-4 col-lg-4 ">
+                                <div class="middleColumn">
+                                    <div class="rule"></div>
+                                    <div class="ruleDot"></div>
+                                    <div class="rule"></div>
+                                </div>
+                            </div>
+                            <div class="col-4 col-md-4 col-lg-4">
+                                <div class="RightColumn ">
+                                </div>
+                            </div>
+                            <div class="col-4 col-md-4 col-lg-4 ">
+                                <div class="leftColumn">
+                                </div>
+                            </div>
+                            <div class="col-4 col-md-4 col-lg-4 ">
+                                <div class="middleColumn">
+                                    <div class="rule"></div>
+                                    <div class="ruleDot"></div>
+                                    <div class="rule"></div>
+                                </div>
+                            </div>
+                            <div class="col-4 col-md-4 col-lg-4 ">
+                                <div class="RightColumn cardBackground alignCenter">
+                                    <p>Unity 3D Game Developer<br>
+                                        Coder House</p>
+                                </div>
+                            </div>
+                            <div class="col-4 col-md-4 col-lg-4 ">
+                            <div class="leftColumn cardBackground alignCenter">
+                                <p>Back-End Developer<br>
+                                    Coder House</p>
+                            </div>
+                        </div>
+                        <div class="col-4 col-md-4 col-lg-4 ">
+                            <div class="middleColumn">
+                                <div class="rule"></div>
+                                <div class="ruleDot"></div>
+                                <div class="rule"></div>
+                            </div>
+                        </div>
+                        <div class="col-4 col-md-4 col-lg-4">
+                            <div class="RightColumn ">
+                            </div>
+                        </div>
+                            
+        `
+    )
+}
+
+function Work() {
+    $('.qualification__content').empty();
+    $('.qualification__content').append(
+        `
+        <div class="col-4 col-md-4 col-lg-4 ">
+                                <div class="leftColumn cardBackground alignCenter">
+                                    <p>Marketing & Copywriting<br>
+                                        MercadoLibre</p>
+                                </div>
+                            </div>
+                            <div class="col-4 col-md-4 col-lg-4 ">
+                                <div class="middleColumn">
+                                    <div class="rule"></div>
+                                    <div class="ruleDot"></div>
+                                    <div class="rule"></div>
+                                </div>
+                            </div>
+                            <div class="col-4 col-md-4 col-lg-4 ">
+                                <div class="RightColumn">
+                                </div>
+                            </div>
+    
+                            <div class="col-4 col-md-4 col-lg-4 ">
+                                <div class="leftColumn">
+    
+                                </div>
+                            </div>
+                            <div class="col-4 col-md-4 col-lg-4 ">
+                                <div class="middleColumn">
+                                    <div class="rule"></div>
+                                    <div class="ruleDot"></div>
+                                    <div class="rule"></div>
+                                </div>
+                            </div>
+                            <div class="col-4 col-md-4 col-lg-4 ">
+                                <div class="RightColumn cardBackground alignCenter">
+                                    <p>SEO & Editor<br>
+                                        Youtube</p>
+                                </div>
+                            </div>
+    
+                            <div class="col-4 col-md-4 col-lg-4 ">
+                                <div class="leftColumn cardBackground alignCenter">
+                                    <p>Front-End Developer<br>
+                                        Coder House</p>
+                                </div>
+                            </div>
+                            <div class="col-4 col-md-4 col-lg-4 ">
+                                <div class="middleColumn">
+                                    <div class="rule"></div>
+                                    <div class="ruleDot"></div>
+                                    <div class="rule"></div>
+                                </div>
+                            </div>
+                            <div class="col-4 col-md-4 col-lg-4">
+                                <div class="RightColumn ">
+                                </div>
+                            </div>
+        `
+    );
+
+}
+
+$(".switch-button::before").append(
+    `
+    <p>sexo</p>
+    `
+)
+
+function switchQualification() {
+    isEducation = !isEducation;
+    if (isEducation) {
+        Education();
+    } else {
+        Work();
+    }
+}
+
+$(".switch-button").click(function () {
+    switchQualification()
+    console.log(isEducation);
+})
+
+$(".contact__label").hide();
+
+$(".contact__input--nombre").focus(function () {
+    $(".contact__label--nombre").show();
+}).focusout(function () {
+    $(".contact__label--nombre").hide();
+});
+
+$(".contact__input--email").focus(function () {
+    $(".contact__label--email").show();
+}).focusout(function () {
+    $(".contact__label--email").hide();
+});
+
+$(".contact__input--asunto").focus(function () {
+    $(".contact__label--asunto").show();
+}).focusout(function () {
+    $(".contact__label--asunto").hide();
+});
+
+$(".contact__input--mensaje").focus(function () {
+    $(".contact__label--mensaje").show();
+}).focusout(function () {
+    $(".contact__label--mensaje").hide();
+});
+
+
+$(".cerrarNavbar").click(function () {
+    $(".navBar").hide();
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Js/navBar.js
@@ -0,0 +1,106 @@
+// minified NavBar
+
+closedNav()
+openedNav()
+$('.openedNav').hide();
+function openedNav() {
+    $('.openedNav').append(
+        `
+        <div class="container-fluid navBar pt-2">
+        <div class="row">
+            <button class="col-4 col-md-4 col-lg-4 py-4 home__button">
+                <span><i class="fas fa-home col-12"></i></span>
+                <span>Home</span>
+            </button>
+            <button class="col-4 col-md-4 col-lg-4 py-4 aboutMe__button">
+                <span><i class="fas fa-user col-12"></i></span>
+                <span>Acerca de Mi</span>
+            </button>
+            <button class="col-4 col-md-4 col-lg-4 py-4 skills__button">
+                <span><i class="fas fa-file-invoice col-12"></i></span>
+                <span>Skills</span>
+            </button>
+    
+            <button class="col-4 col-md-4 col-lg-4 py-4 services__button">
+                <span><i class="fas fa-briefcase col-12"></i></span>
+                <span>Servicios</span>
+            </button>
+            <button class="col-4 col-md-4 col-lg-4 py-4 portfolio__button">
+                <span><i class="fas fa-image col-12"></i></span>
+                <span>Portfolio</span>
+            </button>
+            <button class="col-4 col-md-4 col-lg-4 py-4 contactMe__button">
+                <span><i class="fas fa-comment-alt col-12"></i></span>
+                <span>Contactarme</span>
+            </button>
+    
+            <div class="container-fluid">
+                <div class="row pb-2">
+                    <div class="col-12 col-md-12 col-lg-12 d-flex justify-content-end align-items-center">
+                        <button class="closeNav__button"><span><i class="fas fa-times"></i></span></button>
+                    </div>
+                </div>
+            </div>
+        </div >
+    </div >
+        `
+    )
+}
+
+function closedNav() {
+    $('.closedNav').append(
+        `<div data-aos="fade-up" class="container-fluid navBar__controller pt-2">
+                <div class="row pb-2">
+                    <div class="col-8 col-md-8 col-lg-8 d-flex justify-content-start align-items-center">
+                        <button><span>Esteban Hirzfeld</i></span></button>
+                    </div>
+                    <div class="col-4 col-md-4 col-lg-4 d-flex justify-content-end align-items-center">
+                        <button><span><i class="far fa-moon px-2"></i></i></span></button>
+                        <button class="openNav__button"><span><i class="fas fa-th px-2"></i></span></button>
+                    </div>
+                </div>
+            </div>
+    `)
+}
+
+$('.openNav__button').click(function () {
+    $('.closedNav').hide();
+    $('.openedNav').show();
+})
+
+$('.closeNav__button').click(function () {
+    $('.closedNav').show();
+    $('.openedNav').hide();
+})
+
+
+$('.home__button').click(function () {
+    $('html, body').animate({ scrollTop: $('#home').offset().top }, 100);
+    $('.closedNav').show();
+    $('.openedNav').hide();
+})
+$('.aboutMe__button').click(function () {
+    $('html, body').animate({ scrollTop: $('#aboutMe').offset().top }, 100);
+    $('.closedNav').show();
+    $('.openedNav').hide();
+})
+$('.skills__button').click(function () {
+    $('html, body').animate({ scrollTop: $('#skills').offset().top }, 100);
+    $('.closedNav').show();
+    $('.openedNav').hide();
+})
+$('.services__button').click(function () {
+    $('html, body').animate({ scrollTop: $('#services').offset().top }, 100);
+    $('.closedNav').show();
+    $('.openedNav').hide();
+})
+$('.portfolio__button').click(function () {
+    $('html, body').animate({ scrollTop: $('#portfolio').offset().top }, 100);
+    $('.closedNav').show();
+    $('.openedNav').hide();
+})
+$('.contactMe__button').click(function () {
+    $('html, body').animate({ scrollTop: $('#contactMe').offset().top }, 100);
+    $('.closedNav').show();
+    $('.openedNav').hide();
+})
