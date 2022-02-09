c60daeb3ab876cd29105a93a6907245946fad5cc
estebanhirzfeld <estebanhirzfeld@gmail.com>
2022-02-09 19:33:00 -0300

    some padding n iphone fixes

--- Js/navBar.js
+++ Js/navBar.js
@@ -6,38 +6,38 @@ $('.openedNav').hide();
 function openedNav() {
     $('.openedNav').append(
         `
-        <div class="container-fluid navBar pt-2">
-        <div class="row">
+        <div class="container-fluid navBar  pt-2">
+        <div class="row ">
             <button class="col-4 col-md-4 col-lg-4 py-4 home__button">
-                <span><i class="fas fa-home col-12"></i></span>
-                <span>Home</span>
+                <span><i class="fas fa-home col-12 text-dark"></i></span>
+                <span class="text-dark">Home</span>
             </button>
             <button class="col-4 col-md-4 col-lg-4 py-4 aboutMe__button">
-                <span><i class="fas fa-user col-12"></i></span>
-                <span>Acerca de Mi</span>
+                <span><i class="fas fa-user col-12 text-dark"></i></span>
+                <span class="text-dark">Acerca de Mi</span>
             </button>
             <button class="col-4 col-md-4 col-lg-4 py-4 skills__button">
-                <span><i class="fas fa-file-invoice col-12"></i></span>
-                <span>Skills</span>
+                <span><i class="fas fa-file-invoice col-12 text-dark"></i></span>
+                <span class="text-dark">Skills</span>
             </button>
     
             <button class="col-4 col-md-4 col-lg-4 py-4 services__button">
-                <span><i class="fas fa-briefcase col-12"></i></span>
-                <span>Servicios</span>
+                <span><i class="fas fa-briefcase col-12 text-dark"></i></span>
+                <span class="text-dark">Servicios</span>
             </button>
             <button class="col-4 col-md-4 col-lg-4 py-4 portfolio__button">
-                <span><i class="fas fa-image col-12"></i></span>
-                <span>Portfolio</span>
+                <span><i class="fas fa-image col-12 text-dark"></i></span>
+                <span class="text-dark">Portfolio</span>
             </button>
             <button class="col-4 col-md-4 col-lg-4 py-4 contactMe__button">
-                <span><i class="fas fa-comment-alt col-12"></i></span>
-                <span>Contactarme</span>
+                <span><i class="fas fa-comment-alt col-12 text-dark"></i></span>
+                <span class="text-dark">Contactarme</span>
             </button>
     
             <div class="container-fluid">
                 <div class="row pb-2">
                     <div class="col-12 col-md-12 col-lg-12 d-flex justify-content-end align-items-center">
-                        <button class="closeNav__button"><span><i class="fas fa-times"></i></span></button>
+                        <button class="closeNav__button text-dark"><span><i class="fas fa-times"></i></span></button>
                     </div>
                 </div>
             </div>
@@ -52,11 +52,11 @@ function closedNav() {
         `<div data-aos="fade-up" class="container-fluid navBar__controller py-3">
                 <div class="row">
                     <div class="col-8 col-md-8 col-lg-8 d-flex justify-content-start align-items-center">
-                        <button class="home__button"><span>Esteban Hirzfeld</i></span></button>
+                        <button class="home__button text-dark"><span>Esteban Hirzfeld</i></span></button>
                     </div>
                     <div class="col-4 col-md-4 col-lg-4 d-flex justify-content-end align-items-center">
-                        <button><span><i class="far fa-moon px-2"></i></i></span></button>
-                        <button class="openNav__button"><span><i class="fas fa-th px-2"></i></span></button>
+                        <button><span><i class="far fa-moon px-2 text-dark"></i></i></span></button>
+                        <button class="openNav__button text-dark"><span><i class="fas fa-th px-2"></i></span></button>
                     </div>
                 </div>
             </div>
